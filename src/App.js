46d8e5412ecf4c214c4c9fd29d314947d1ddd714
BayCity-Jeff <149318778+BayCity-Jeff@users.noreply.github.com>
2024-01-05 11:06:31 -0500

    Structure added and semantic corrections made.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header>
+        <img src="logo.svg" />
+        <div>Nav.js</div>
+      </Header>
+      <Nav>
+        <ul>
+          <li><a href="/home">HOME</a></li>
+          <li><a href="/about">ABOUT</a></li>
+          <li><a href="/blog">MENU</a></li>
+          <li><a href="/reservations">RESERVATIONS</a></li>
+          <li><a href="/order">ORDER ONLINE</a></li>
+          <li><a href="/login">LOGIN</a></li>
+        </ul>
+      </Nav>
+      <Main>
+        M
+      </Main>
+      <Footer>
+        <meta property='og:image' content='/logo.svg' />
+        <div>Nav.js</div>
+        <div>Contact element</div>
+        <div>Soc element</div>
+      </Footer>
+    </>
   );
 }
 
